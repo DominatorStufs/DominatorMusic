mikropsoft/ViTune

Add predictive back

compose/routing/src/main/kotlin/app/vitune/compose/routing/RouteHandler.kt
@@ -1,13 +1,14 @@
 package app.vitune.compose.routing
 
-import androidx.activity.compose.BackHandler
 import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.AnimatedContentTransitionScope
 import androidx.compose.animation.ContentTransform
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.animation.core.updateTransition
+import androidx.compose.animation.core.SeekableTransitionState
+import androidx.compose.animation.core.rememberTransition
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -19,9 +20,8 @@ import androidx.compose.ui.Modifier
 @Composable
 fun RouteHandler(
     modifier: Modifier = Modifier,
-    listenToGlobalEmitter: Boolean = false,
-    handleBackPress: Boolean = true,
-    transitionSpec: AnimatedContentTransitionScope<RouteHandlerScope>.() -> ContentTransform = {
+    listenToGlobalEmitter: Boolean = true,
+    transitionSpec: AnimatedContentTransitionScope<Route?>.() -> ContentTransform = {
         when {
             isStacking -> defaultStacking
             isStill -> defaultStill
@@ -38,7 +38,6 @@ fun RouteHandler(
         route = route,
         onRouteChanged = { route = it },
         listenToGlobalEmitter = listenToGlobalEmitter,
-        handleBackPress = handleBackPress,
         transitionSpec = transitionSpec,
         modifier = modifier,
         content = content
@@ -52,8 +51,7 @@ fun RouteHandler(
     onRouteChanged: (Route?) -> Unit,
     modifier: Modifier = Modifier,
     listenToGlobalEmitter: Boolean = false,
-    handleBackPress: Boolean = true,
-    transitionSpec: AnimatedContentTransitionScope<RouteHandlerScope>.() -> ContentTransform = {
+    transitionSpec: AnimatedContentTransitionScope<Route?>.() -> ContentTransform = {
         when {
             isStacking -> defaultStacking
             isStill -> defaultStill
@@ -63,36 +61,54 @@ fun RouteHandler(
     content: @Composable RouteHandlerScope.() -> Unit
 ) {
     val backDispatcher = LocalOnBackPressedDispatcherOwner.current?.onBackPressedDispatcher
+    val parameters = rememberSaveable { arrayOfNulls<Any?>(3) }
 
-    val parameters = rememberSaveable {
-        arrayOfNulls<Any?>(3)
+    if (listenToGlobalEmitter && route == null) OnGlobalRoute { (newRoute, newParameters) ->
+        newParameters.forEachIndexed(parameters::set)
+        onRouteChanged(newRoute)
     }
 
-    val scope = remember(route) {
-        RouteHandlerScope(
-            route = route,
-            parameters = parameters,
-            push = onRouteChanged,
-            pop = { if (handleBackPress) backDispatcher?.onBackPressed() else onRouteChanged(null) }
+    var predictiveBackProgress: Float? by remember { mutableStateOf(null) }
+    GlobalPredictiveBackHandler(
+        enabled = route != null,
+        onStart = { predictiveBackProgress = 0f },
+        onProgress = { predictiveBackProgress = it },
+        onFinish = {
+            onRouteChanged(null)
+            predictiveBackProgress = null
+        },
+        onCancel = {
+            predictiveBackProgress = null
+        }
+    )
+
+    fun Route?.scope() = RouteHandlerScope(
+        route = this,
+        parameters = parameters,
+        push = onRouteChanged,
+        pop = {
+            backDispatcher?.onBackPressed()
+        }
+    )
+
+    val transitionState = remember { SeekableTransitionState(route) }
+
+    if (predictiveBackProgress == null) LaunchedEffect(route) {
+        if (transitionState.currentState != route) transitionState.animateTo(route)
+    } else LaunchedEffect(predictiveBackProgress) {
+        transitionState.seekTo(
+            fraction = predictiveBackProgress ?: 0f,
+            targetState = null
         )
     }
 
-    if (listenToGlobalEmitter && route == null) {
-        OnGlobalRoute { (newRoute, newParameters) ->
-            newParameters.forEachIndexed(parameters::set)
-            onRouteChanged(newRoute)
-        }
-    }
-
-    BackHandler(enabled = handleBackPress && route != null) {
-        onRouteChanged(null)
-    }
-
-    updateTransition(targetState = scope, label = null).AnimatedContent(
+    rememberTransition(
+        transitionState = transitionState,
+        label = null
+    ).AnimatedContent(
         transitionSpec = transitionSpec,
-        contentKey = RouteHandlerScope::route,
         modifier = modifier
     ) {
-        it.content()
+        it.scope().content()
     }
 }

compose/routing/src/main/kotlin/app/vitune/compose/routing/Transitions.kt
@@ -8,39 +8,29 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.scaleOut
 
-@ExperimentalAnimationApi
 val defaultStacking = ContentTransform(
     initialContentExit = scaleOut(targetScale = 0.9f) + fadeOut(),
     targetContentEnter = fadeIn(),
     targetContentZIndex = 1f
 )
 
-@ExperimentalAnimationApi
 val defaultUnstacking = ContentTransform(
-    initialContentExit = fadeOut(),
+    initialContentExit = scaleOut(targetScale = 1.1f) + fadeOut(),
     targetContentEnter = EnterTransition.None,
     targetContentZIndex = 0f
 )
 
-@ExperimentalAnimationApi
 val defaultStill = ContentTransform(
     initialContentExit = scaleOut(targetScale = 0.9f) + fadeOut(),
     targetContentEnter = fadeIn(),
     targetContentZIndex = 1f
 )
 
-@ExperimentalAnimationApi
-val AnimatedContentTransitionScope<RouteHandlerScope>.isStacking: Boolean
-    get() = initialState.route == null && targetState.route != null
+val AnimatedContentTransitionScope<Route?>.isStacking: Boolean
+    get() = initialState == null && targetState != null
 
-@ExperimentalAnimationApi
-val AnimatedContentTransitionScope<RouteHandlerScope>.isUnstacking: Boolean
-    get() = initialState.route != null && targetState.route == null
+val AnimatedContentTransitionScope<Route?>.isUnstacking: Boolean
+    get() = initialState != null && targetState == null
 
-@ExperimentalAnimationApi
-val AnimatedContentTransitionScope<RouteHandlerScope>.isStill: Boolean
-    get() = initialState.route == null && targetState.route == null
-
-@ExperimentalAnimationApi
-val AnimatedContentTransitionScope<RouteHandlerScope>.isUnknown: Boolean
-    get() = initialState.route != null && targetState.route != null
+val AnimatedContentTransitionScope<Route?>.isStill: Boolean
+    get() = initialState == null && targetState == null

DominatorStufs/DominatorMusic

compose/routing/src/main/kotlin/app/vitune/compose/routing/RouteHandler.kt
@@ -1,12 +1,13 @@
 package app.vitune.compose.routing
 
-import androidx.activity.compose.BackHandler
 import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.AnimatedContentTransitionScope
 import androidx.compose.animation.ContentTransform
-import androidx.compose.animation.core.updateTransition
+import androidx.compose.animation.core.SeekableTransitionState
+import androidx.compose.animation.core.rememberTransition
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -17,7 +18,7 @@ import androidx.compose.ui.Modifier
 typealias TransitionScope<T> = AnimatedContentTransitionScope<T>
 typealias TransitionSpec<T> = TransitionScope<T>.() -> ContentTransform
 
-private val defaultTransitionSpec: TransitionSpec<RouteHandlerScope> = {
+private val defaultTransitionSpec: TransitionSpec<Route?> = {
     when {
         isStacking -> defaultStacking
         isStill -> defaultStill
@@ -28,9 +29,8 @@ private val defaultTransitionSpec: TransitionSpec<RouteHandlerScope> = {
 @Composable
 fun RouteHandler(
     modifier: Modifier = Modifier,
-    listenToGlobalEmitter: Boolean = false,
-    handleBackPress: Boolean = true,
-    transitionSpec: TransitionSpec<RouteHandlerScope> = defaultTransitionSpec,
+    listenToGlobalEmitter: Boolean = true,
+    transitionSpec: TransitionSpec<Route?> = defaultTransitionSpec,
     content: @Composable RouteHandlerScope.() -> Unit
 ) {
     var route by rememberSaveable(stateSaver = Route.Saver) {
@@ -41,7 +41,6 @@ fun RouteHandler(
         route = route,
         onRouteChange = { route = it },
         listenToGlobalEmitter = listenToGlobalEmitter,
-        handleBackPress = handleBackPress,
         transitionSpec = transitionSpec,
         modifier = modifier,
         content = content
@@ -53,37 +52,59 @@ fun RouteHandler(
     route: Route?,
     onRouteChange: (Route?) -> Unit,
     modifier: Modifier = Modifier,
-    listenToGlobalEmitter: Boolean = false,
-    handleBackPress: Boolean = true,
-    transitionSpec: TransitionSpec<RouteHandlerScope> = defaultTransitionSpec,
+    listenToGlobalEmitter: Boolean = true,
+    transitionSpec: TransitionSpec<Route?> = defaultTransitionSpec,
     content: @Composable RouteHandlerScope.() -> Unit
 ) {
     val backDispatcher = LocalOnBackPressedDispatcherOwner.current?.onBackPressedDispatcher
-
     val parameters = rememberSaveable { arrayOfNulls<Any?>(4) }
-    val scope = remember(route) {
-        RouteHandlerScope(
-            route = route,
-            parameters = parameters,
-            push = onRouteChange,
-            pop = { if (handleBackPress) backDispatcher?.onBackPressed() else onRouteChange(null) }
+
+    if (listenToGlobalEmitter && route == null) OnGlobalRoute { newRouteRequest ->
+        newRouteRequest.args.forEachIndexed(parameters::set)
+        onRouteChange(newRouteRequest.route)
+    }
+
+    var predictiveBackProgress: Float? by remember { mutableStateOf(null) }
+    GlobalPredictiveBackHandler(
+        enabled = route != null,
+        onStart = { predictiveBackProgress = 0f },
+        onProgress = { predictiveBackProgress = it },
+        onFinish = {
+            onRouteChange(null)
+            predictiveBackProgress = null
+        },
+        onCancel = {
+            predictiveBackProgress = null
+        }
+    )
+
+    fun Route?.scope() = RouteHandlerScope(
+        route = this,
+        parameters = parameters,
+        push = onRouteChange,
+        pop = {
+            backDispatcher?.onBackPressed()
+        }
+    )
+
+    val transitionState = remember { SeekableTransitionState(route) }
+
+    if (predictiveBackProgress == null) LaunchedEffect(route) {
+        if (transitionState.currentState != route) transitionState.animateTo(route)
+    } else LaunchedEffect(predictiveBackProgress) {
+        transitionState.seekTo(
+            fraction = predictiveBackProgress ?: 0f,
+            targetState = null
         )
     }
 
-    if (listenToGlobalEmitter && route == null) OnGlobalRoute { request ->
-        request.args.forEachIndexed(parameters::set)
-        onRouteChange(request.route)
-    }
-
-    BackHandler(enabled = handleBackPress && route != null) {
-        onRouteChange(null)
-    }
-
-    updateTransition(targetState = scope, label = null).AnimatedContent(
+    rememberTransition(
+        transitionState = transitionState,
+        label = null
+    ).AnimatedContent(
         transitionSpec = transitionSpec,
-        contentKey = RouteHandlerScope::route,
         modifier = modifier
     ) {
-        it.content()
+        it.scope().content()
     }
 }

compose/routing/src/main/kotlin/app/vitune/compose/routing/Transitions.kt
@@ -13,7 +13,7 @@ val defaultStacking = ContentTransform(
 )
 
 val defaultUnstacking = ContentTransform(
-    initialContentExit = fadeOut(),
+    initialContentExit = scaleOut(targetScale = 1.1f) + fadeOut(),
     targetContentEnter = EnterTransition.None,
     targetContentZIndex = 0f
 )
@@ -24,14 +24,11 @@ val defaultStill = ContentTransform(
     targetContentZIndex = 1f
 )
 
-val TransitionScope<RouteHandlerScope>.isStacking
-    get() = initialState.route == null && targetState.route != null
+val TransitionScope<Route?>.isStacking: Boolean
+    get() = initialState == null && targetState != null
 
-val TransitionScope<RouteHandlerScope>.isUnstacking
-    get() = initialState.route != null && targetState.route == null
+val TransitionScope<Route?>.isUnstacking: Boolean
+    get() = initialState != null && targetState == null
 
-val TransitionScope<RouteHandlerScope>.isStill
-    get() = initialState.route == null && targetState.route == null
-
-val TransitionScope<RouteHandlerScope>.isUnknown
-    get() = initialState.route != null && targetState.route != null
+val TransitionScope<Route?>.isStill: Boolean
+    get() = initialState == null && targetState == null